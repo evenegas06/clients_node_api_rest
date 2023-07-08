2c516c514de5753b629dc1d927d601ac5676bccf
Eduardo Venegas <evenegas06@hotmail.com>
2023-07-08 14:48:48 -0600

    mongodb connection

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import mongoose from "mongoose";
 
-import router from './routes/index.js';
+import api_routes from './routes/api.js';
 
+/* ----- Create app ----- */
 const app = express();
 
-app.use('/', router);
+/* ----- DB connection ----- */
+mongoose.connect('mongodb://127.0.0.1:27017/node_api_rssssest')
+    .then(() => console.log('Database connected'))
+    .catch((error) => console.error(error));
+
+
+app.use('/api', api_routes);
 
 app.listen(5000, () => {
     console.log(`Server listening on port 500 -> http://localhost:5000`);
